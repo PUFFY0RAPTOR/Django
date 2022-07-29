4e41cc07efe0b2214c64e9d122bfd27520cf7936
SebasSitioWeb <sebastianpertuzg@hotmail.com>
2022-07-29 11:46:50 -0500

    cositas de python

--- territorio/admin.py
+++ territorio/admin.py
@@ -1,7 +1,35 @@
 from django.contrib import admin
 
-from .models import Aprendiz
+from .models import Aprendiz, Monitoria, Actividades
 
 # Register your models here.
 
-admin.site.register(Aprendiz)
+@admin.register(Aprendiz)
+class AprendizAdmin(admin.ModelAdmin):
+    list_display = ('cedula', 'nombre', 'apellido', 'fecha_nacimiento', 'edad_fecha')
+    search_fields = ['cedula', 'nombre', 'apellido', 'fecha_nacimiento']
+
+    def edad_fecha(self, obj):
+        ed = obj.fecha_nacimiento
+        return ed
+
+ 
+@admin.register(Monitoria)
+class MonitoriaAdmin(admin.ModelAdmin):
+    list_display = ('cat', 'cedula_Aprendiz', 'aprendiz', 'apellido_Aprendiz', 'fecha_inicio', 'fecha_final')
+    search_fields = ['cat', 'aprendiz__nombre', 'aprendiz__apellido', 'aprendiz__cedula']
+
+    def apellido_Aprendiz(self, obj):
+        return obj.aprendiz.apellido
+
+    def cedula_Aprendiz(self, obj):
+        return obj.aprendiz.cedula
+
+
+@admin.register(Actividades)
+class ActividadesAdmin():
+    pass
+
+#admin.site.register(Aprendiz, AprendizAdmin)
+#admin.site.register(Monitoria)
+#admin.site.register(Actividades)
